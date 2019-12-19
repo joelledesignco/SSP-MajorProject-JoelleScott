efba8c39e891d702ece69188d075213ddf38edac
Joelle Scott <hello@joelledesignco.com>
2019-12-18 18:56:57 -0800

    Update header.php

--- header.php
+++ header.php
@@ -17,7 +17,7 @@ require_once("conn.php");
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
-    <title>Hello, world!</title>
+    <title>Sticks & Stones</title>
   </head>
   <body>
 
